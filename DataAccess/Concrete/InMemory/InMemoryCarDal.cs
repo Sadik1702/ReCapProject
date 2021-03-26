ed80800ed724101f6c0cd3127847fd4e3e6372fb
Sadık <sadiksezgin1702@hotmail.com>
2021-03-26 15:52:55 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿//using DataAccess.Abstract;
+//using Entities.Concrete;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace DataAccess.Concrete.InMemory
+//{
+//    public class InMemoryCarDal : ICarDal
+//    {
+//        List<Car> _cars;
+//        public InMemoryCarDal()
+//        {
+//            _cars = new List<Car> {
+//                new Car{CarId= 1, BrandId = 1, ColorId = 1, DailyPrice = 200, ModelYear = "2012", Descriptions = "Otomatik Dizel" },    
+//                new Car{CarId= 2, BrandId = 2, ColorId = 2, DailyPrice = 50, ModelYear = "2010", Descriptions = "Manuel Benzin" },
+//                new Car{CarId= 3, BrandId = 2, ColorId = 1, DailyPrice = 100, ModelYear = "2019", Descriptions = "Manuel Dizel" },
+//                new Car{CarId= 4, BrandId = 3, ColorId = 2, DailyPrice = 150, ModelYear = "2018", Descriptions = "Otomatik Benzin" },
+//            };
+//        }
+//        public void Add(Car car)
+//        {
+//            _cars.Add(car);
+//        }
+
+//        public void Delete(Car car)
+//        {
+//            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+//            _cars.Remove(carToDelete);
+//        }
+
+//        public List<Car> GetAll()
+//        {
+//            return _cars;
+//        }
+        
+//        public List<Car> GetById(int id)
+//        {
+//            return _cars.Where(c => c.CarId == id).ToList();
+//        }
+
+//        public void Update(Car car)
+//        {
+//            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+//            carToUpdate.CarId = car.CarId;
+//            carToUpdate.BrandId = car.BrandId;
+//            carToUpdate.ColorId = car.ColorId;
+//            carToUpdate.ModelYear = car.ModelYear;
+//            carToUpdate.DailyPrice = car.DailyPrice;
+//            carToUpdate.Descriptions = car.Descriptions;
+//        }
+//    }
+//}
---
+++ SQLQuery1.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE Cars (
+CarId int,
+BrandId int,
+ColorId int,
+ModelYear varchar(255),
+DailyPrice decimal,
+Descriptions varchar(255),
+);
+
+INSERT INTO Cars (CarId, BrandId, ColorId, ModelYear, DailyPrice, Descriptions)
+VALUES (1, 1, 1, 1970, 350, 'Dodge Challenger'),
+(2, 2, 2, 2002, 150, 'Toyota Yaris'),
+(3, 3, 3, 2015, 400, 'Ford Ranger');
+
+
+
+CREATE TABLE Brands (
+BrandId int,
+BrandName varchar(255),
+);
+
+INSERT INTO Brands (BrandId, BrandName)
+Values (1, 'Dodge'),
+(2, 'Toyota'),
+(3, 'Ford');
+
+
+
+
+
+CREATE TABLE Colors (
+ColorId int,
+ColorName varchar(255),
+);
+
+INSERT INTO Colors (ColorId, ColorName)
+Values (1, 'Siyah'),
+(2, 'Beyaz'),
+(3, 'Gri');
